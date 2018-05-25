d553b6a7d91a0cef58cb5b65df6f6d3e689a4b29
JuliusDorfman <juliusdorfman@gmail.com>
2018-05-24 22:16:49 -0700

    create build to push to heroku

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,10 @@
-module.exports = {
-	mongoURI: 'mongodb://Julius:032491jgcd@ds129560.mlab.com:29560/news-from-db'
+if (process.env.NODE_ENV === 'production') {
+    module.exports = require('./keys_prod');
+} else {
+    module.exports = require('./keys_dev');
 }
+
+
+// module.exports = {
+// 	mongoURI: 'mongodb://Julius:032491jgcd@ds129560.mlab.com:29560/news-from-db'
+// }
