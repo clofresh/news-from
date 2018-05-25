327aa6f337f7aa5630c2c468a6500a94d0b7d02e
JuliusDorfman <juliusdorfman@gmail.com>
2018-05-25 13:54:12 -0700

    server.js static pages heroku deploy try

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use('/', (req, res) => {
 
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('./build'))
+    app.use(express.static('/build'))
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     });
